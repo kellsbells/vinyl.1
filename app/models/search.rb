8c11c52be729e9212f563b8e686499363c82c180
kellsbells <kelleeballard@hotmail.com>
2014-09-10 23:16:54 -0600

    Sets up Search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,1 +1,8 @@
-search.rb
+class Search
+  def self.for(keyword)
+    keyword_search = "%#{keyword.downcase}%"
+
+    Post.where('lower(album_name) LIKE ?', keyword_search) +
+      Post.where('lower(artist) LIKE ?', keyword_search)
+  end
+end
