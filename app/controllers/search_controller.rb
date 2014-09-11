5df6a6fcf0350d3fcea574aa34987499880fdb77
kellsbells <kelleeballard@hotmail.com>
2014-09-10 23:16:28 -0600

    Sets Serach controller and routing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,1 +1,14 @@
-search_controller.rb
+class SearchController < ApplicationController
+  def index
+    keyword = params[:keyword]
+
+    if artist = Post.find_by(artist: keyword)
+      redirect_to artist
+    elsif album_name = Post.find_by(album_name: keyword)
+      redirect_to album_name
+    else
+      @results = Search.for(keyword)
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/index'
 
   resources :posts
+  get 'search' => 'search#index', as: :search
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
   devise_for :admin_users, ActiveAdmin::Devise.config
