c8c5ba52389b12adaa77b1efd1b17e041be4d461
kellsbells <kelleeballard@hotmail.com>
2014-09-12 23:20:49 -0600

    Modifies the search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,6 @@
 class SearchController < ApplicationController
   def index
     keyword = params[:keyword]
-
-    if artist = Post.find_by(artist: keyword)
-      redirect_to artist
-    elsif album_name = Post.find_by(album_name: keyword)
-      redirect_to album_name
-    else
-      @results = Search.for(keyword)
-    end
-    
+    @results = Search.for(keyword) 
   end
 end
